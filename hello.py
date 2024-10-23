080161270b0a0606ff8eb88f9bdeed27a7ba8973
tonegi <90652377+tonegi@users.noreply.github.com>
2024-10-23 10:22:08 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,29 @@
-# A very simple Flask Hello World app for you to get started with...
-from flask import Flask
+from datetime import datetime
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/')
-def hello_world():
-    return '<p>Alterações por meio do GitHub</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
+def index():
+     return render_template('index.html', current_time=datetime.utcnow())
+
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
